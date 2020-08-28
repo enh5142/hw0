531066476871d7ba8484fc6d2ccc6b91c3620d5c
enh5142 <enh5142@psu.edu>
2020-08-28 22:19:10 +0000

    changed int to float

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-temp = int(input('Enter temperature in celsius: '))
+temp = float(input('Enter temperature in celsius: '))
 m = (temp * (9/5)) + 32
 print(str(temp) + '° in Celsius is equivalent to ' + str(m) + '° fahrenheit ')
